7d3193365240f5e1ddff80aeecb6e09bdb3f148d
ingenieria <andres.bedoya@upb.edu.co>
2019-03-19 11:05:58 -0500

    Se crea la interfaz del menu

--- AsciiMenu/Program.cs
+++ AsciiMenu/Program.cs
@@ -10,14 +10,30 @@ namespace AsciiMenu
     {
         static void Main(string[] args)
         {
-            /* Imprimiendo una cade con comillas
-               si intenta imprimir Console.WrtiLine(""hola"");
-               aparece un error de sintaxis, solución usar
-               una secuencia de escape
-            */
 
-            Console.WriteLine("\"hola\"");
-            Console.WriteLine("\\hola\\");
+            Console.WriteLine("  ...............................q(-_-)p.................................");
+            Console.WriteLine("  .   o   \\ o /  _ o        __|  '_) (_`   |__         o _  \\ o /   o   .");
+            Console.WriteLine("  .  /|\\    |     /\\   __\\o      /__/  \\        o/__   /\\     |    /|\\  .");
+            Console.WriteLine("  .  / \\   / \\   | \\  /) |     _(<_   / )_      |   (\\ / |   / \\   / \\  .");
+            Console.WriteLine("  .       ....................(__\\_\\_|_/__)......................       .");
+            Console.WriteLine("  .       .    ___            _                                 .       .");
+            Console.WriteLine("  .       .   / __\\___   ___ | |       /\\/\\   ___ _ __  _   _   .       .");
+            Console.WriteLine("  .  __\\o .  / /  / _ \\ / _ \\| |_____ /    \\ / _ \\ '_ \\| | | |  .  __\\o .");
+            Console.WriteLine("  . /) |  . / /__| (_) | (_) | |_____/ /\\/\\ \\  __/ | | | |_| |  . /) |  .");
+            Console.WriteLine("  .       . \\____/\\___/ \\___/|_|     \\/    \\/\\___|_| |_|\\__,_|  .       .");
+            Console.WriteLine("  .  o _  .                                                     .  o _  .");
+            Console.WriteLine("  .  /\\   .              Ingrese una opción así:                .  /\\   .");
+            Console.WriteLine("  .  / |  .   1. Dibujar un cuadrado de N por M líneas          .  / |  .");
+            Console.WriteLine("  .       .   2. Traducir un emoji a una expresión escrita      .       .");
+            Console.WriteLine("  . \\ o / .   3. Códificar un texto usando la clave murciélago  . \\ o / .");
+            Console.WriteLine("  .   |   .  -1. Para salir                                     .   |   .");
+            Console.WriteLine("  .  / \\  .                                                     .  / \\  .");
+            Console.WriteLine("  .       .......................................................       .");
+            Console.WriteLine("  .   o   \\ o /  _ o        __|    \\ /     |__         o _  \\ o /   o   .");
+            Console.WriteLine("  .  /|\\    |     /\\   __\\o   \\o    |    o/     o/__   /\\     |    /|\\  .");
+            Console.WriteLine("  .  / \\   / \\   | \\  /) |    ( \\  /o\\  / )    |   (\\  / |   / \\   / \\  .");
+            Console.WriteLine("  dc.....................................................................");
+
 
             //Esta línea es para que no se cierre el programa si no lo ejecuta con ctrl + f5
             Console.ReadKey();
